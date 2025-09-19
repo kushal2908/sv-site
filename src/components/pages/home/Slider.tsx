12c50abc9974429129d61d10c81cbf0427a8b576
Sarder Safa Bin Salam <32867858+kushal2908@users.noreply.github.com>
2025-09-19 14:11:32 +0600

    updated

--- src/components/pages/home/Slider.tsx
+++ src/components/pages/home/Slider.tsx
@@ -1,132 +1,132 @@
-'use client';
-import React from 'react';
-import Image from 'next/image';
-import { Autoplay } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 export default function LogoSlider() {
-    return (
-        <div className="min-h-[300px] flex items-center justify-center">
-            <Swiper
-                modules={[Autoplay]}
-                spaceBetween={10}
-                slidesPerView={6}
-                loop={true}
-                autoplay={{
-                    delay: 1000,
-                    disableOnInteraction: false,
-                }}
-            >
-                <SwiperSlide>
-                    <Image
-                        src="/slider/apple-tv-logo.png"
-                        alt="skill-ventrure-clients"
-                        width={1920}
-                        height={200}
-                        className="w-full h-[100px] object-contain"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/slider/canal-hd-logo.png"
-                        alt="skill-ventrure-clients"
-                        width={1920}
-                        height={200}
-                        className="w-full h-[100px] object-contain"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/slider/hbo-max-black-logo.png"
-                        alt="skill-ventrure-clients"
-                        width={1920}
-                        height={200}
-                        className="w-full h-[100px] object-contain"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/slider/pepe-jense.png"
-                        alt="skill-ventrure-clients"
-                        width={1920}
-                        height={200}
-                        className="w-full h-[100px] object-contain"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/slider/dkny.png"
-                        alt="skill-ventrure-clients"
-                        width={1920}
-                        height={200}
-                        className="w-full h-[100px] object-contain"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/slider/mango.png"
-                        alt="skill-ventrure-clients"
-                        width={1920}
-                        height={200}
-                        className="w-full h-[100px] object-contain"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/slider/adolf.png"
-                        alt="skill-ventrure-clients"
-                        width={1920}
-                        height={200}
-                        className="w-full h-[100px] object-contain"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/slider/adidas.png"
-                        alt="skill-ventrure-clients"
-                        width={1920}
-                        height={200}
-                        className="w-full h-[100px] object-contain"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/slider/inditex.png"
-                        alt="skill-ventrure-clients"
-                        width={1920}
-                        height={200}
-                        className="w-full h-[100px] object-contain"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/slider/s-oliver.png"
-                        alt="skill-ventrure-clients"
-                        width={1920}
-                        height={200}
-                        className="w-full h-[100px] object-contain"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/slider/gap.png"
-                        alt="skill-ventrure-clients"
-                        width={1920}
-                        height={200}
-                        className="w-full h-[100px] object-contain"
-                    />
-                </SwiperSlide>{' '}
-                <SwiperSlide>
-                    <Image
-                        src="/slider/zalando.png"
-                        alt="skill-ventrure-clients"
-                        width={1920}
-                        height={200}
-                        className="w-full h-[100px] object-contain"
-                    />
-                </SwiperSlide>
-            </Swiper>
-        </div>
-    );
+  return (
+    <div className="min-h-[300px] flex items-center justify-center">
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={10}
+        slidesPerView={6}
+        loop={true}
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+      >
+        <SwiperSlide>
+          <Image
+            src="/slider/1.png"
+            alt="skill-ventrure-clients"
+            width={1920}
+            height={200}
+            className="w-full h-[100px] object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/slider/3.png"
+            alt="skill-ventrure-clients"
+            width={1920}
+            height={200}
+            className="w-full h-[100px] object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/slider/4.jpg"
+            alt="skill-ventrure-clients"
+            width={1920}
+            height={200}
+            className="w-full h-[100px] object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/slider/5.png"
+            alt="skill-ventrure-clients"
+            width={1920}
+            height={200}
+            className="w-full h-[100px] object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/slider/6.jpg"
+            alt="skill-ventrure-clients"
+            width={1920}
+            height={200}
+            className="w-full h-[100px] object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/slider/7.jpg"
+            alt="skill-ventrure-clients"
+            width={1920}
+            height={200}
+            className="w-full h-[100px] object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/slider/8.png"
+            alt="skill-ventrure-clients"
+            width={1920}
+            height={200}
+            className="w-full h-[100px] object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/slider/9.jpg"
+            alt="skill-ventrure-clients"
+            width={1920}
+            height={200}
+            className="w-full h-[100px] object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/slider/10.png"
+            alt="skill-ventrure-clients"
+            width={1920}
+            height={200}
+            className="w-full h-[100px] object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/slider/11.jpg"
+            alt="skill-ventrure-clients"
+            width={1920}
+            height={200}
+            className="w-full h-[100px] object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/slider/12.jpg"
+            alt="skill-ventrure-clients"
+            width={1920}
+            height={200}
+            className="w-full h-[100px] object-contain"
+          />
+        </SwiperSlide>{" "}
+        <SwiperSlide>
+          <Image
+            src="/slider/1.png"
+            alt="skill-ventrure-clients"
+            width={1920}
+            height={200}
+            className="w-full h-[100px] object-contain"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
--- src/components/pages/home/services.tsx
+++ src/components/pages/home/services.tsx
@@ -1,150 +1,162 @@
-import AnimatedCard from '@/components/blocks/AnimatedCard';
-import ServiceImage from '@/components/blocks/ServiceImage';
-import React from 'react';
-import ServiceMobile from './service-mobile';
+import AnimatedCard from "@/components/blocks/AnimatedCard";
+import ServiceImage from "@/components/blocks/ServiceImage";
+import React from "react";
+import ServiceMobile from "./service-mobile";
 
 export default function Services() {
-    return (
-        <>
-            {/* Desktop view */}
-            <div className="max-w-[1600px] mx-auto hidden lg:grid grid-cols-1 lg:grid-cols-4 gap-4 p-4 pb-14">
-                <ColumnOne />
-                <ColumnTwo />
-                <ColumnThree />
-                <ColumnFour />
-            </div>
-            {/* Mobile view */}
-            <div className="mx-auto px-4 block lg:hidden">
-                <ServiceMobile />
-            </div>
-        </>
-    );
+  return (
+    <>
+      {/* Desktop view */}
+      <div className="max-w-[1600px] mx-auto hidden lg:grid grid-cols-1 lg:grid-cols-4 gap-4 p-4 pb-14">
+        <ColumnOne />
+        <ColumnTwo />
+        <ColumnThree />
+        <ColumnFour />
+      </div>
+      {/* Mobile view */}
+      <div className="mx-auto px-4 block lg:hidden">
+        <ServiceMobile />
+      </div>
+    </>
+  );
 }
 
 const ColumnOne = () => {
-    return (
-        <div className="flex flex-col gap-4 items-center justify-center">
-            <ServiceImage src={'/brands/skill-graphics/skill-graphics-1.png'} />
-            <ServiceImage src={'/brands/skill-graphics/skill-graphics-3.jpg'} />
-            <AnimatedCard
-                title={
-                    <>
-                        V2 <br /> Technologies
-                    </>
-                }
-                description={
-                    <>
-                        <p className="transition-all duration-700 ease-out delay-100 group-hover:translate-y-0">
-                            Customized Software <br /> Development
-                        </p>
-                        <p className="transition-all duration-700 ease-out delay-200 group-hover:translate-y-0">
-                            Data Analytics & <br /> Visualization
-                        </p>
-                    </>
-                }
-                url="https://v2.ltd/"
-            />
-            <ServiceImage src={'/brands/v2/v2-1.png'} />
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <ServiceImage src={"/brands/skill-graphics/sk-1.jpg"} />
+      <ServiceImage src={"/brands/skill-graphics/sk-3.jpg"} />
+      <AnimatedCard
+        title={
+          <>
+            V2 <br /> Technologies
+          </>
+        }
+        description={
+          <>
+            <p className="transition-all duration-700 ease-out delay-100 group-hover:translate-y-0">
+              Customized Software <br /> Development
+            </p>
+            <p className="transition-all duration-700 ease-out delay-200 group-hover:translate-y-0">
+              Data Analytics & <br /> Visualization
+            </p>
+          </>
+        }
+        url="https://v2.ltd/"
+      />
+      <ServiceImage src={"/brands/v2/v2-1.png"} />
+    </div>
+  );
 };
 
 const ColumnTwo = () => {
-    return (
-        <div className="flex flex-col gap-4 items-center justify-center">
-            <AnimatedCard
-                title={
-                    <>
-                        Skill <br /> Graphics
-                    </>
-                }
-                description={
-                    <>
-                        <p className="transition-all duration-700 ease-out delay-100 group-hover:translate-y-0">Image Editing</p>
-                        <p className="transition-all duration-700 ease-out delay-200 group-hover:translate-y-0">Video Editing</p>
-                    </>
-                }
-                url="https://skillgraphics.biz/"
-            />
-            <ServiceImage src={'/brands/mvrk/mvrk-1.jpg'} />
-            <AnimatedCard
-                title={<>AULEEK</>}
-                description={
-                    <>
-                        <p className="transition-all duration-700 ease-out delay-100 group-hover:translate-y-0">
-                            Architectural & <br />
-                            Product Visualization
-                        </p>
-                        <p className="transition-all duration-700 ease-out delay-200 group-hover:translate-y-0">
-                            Data Analytics & <br /> Visualization
-                        </p>
-                    </>
-                }
-                url="https://auleek.com/"
-            />
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <AnimatedCard
+        title={
+          <>
+            Skill <br /> Graphics
+          </>
+        }
+        description={
+          <>
+            <p className="transition-all duration-700 ease-out delay-100 group-hover:translate-y-0">
+              Image Editing
+            </p>
+            <p className="transition-all duration-700 ease-out delay-200 group-hover:translate-y-0">
+              Video Editing
+            </p>
+          </>
+        }
+        url="https://skillgraphics.biz/"
+      />
+      <ServiceImage src={"/brands/mvrk/mvrk-1.jpg"} />
+      <AnimatedCard
+        title={<>AULEEK</>}
+        description={
+          <>
+            <p className="transition-all duration-700 ease-out delay-100 group-hover:translate-y-0">
+              Architectural & <br />
+              Product Visualization
+            </p>
+            <p className="transition-all duration-700 ease-out delay-200 group-hover:translate-y-0">
+              Data Analytics & <br /> Visualization
+            </p>
+          </>
+        }
+        url="https://auleek.com/"
+      />
+    </div>
+  );
 };
 
 const ColumnThree = () => {
-    return (
-        <div className="flex flex-col gap-4 items-center justify-center">
-            <ServiceImage src={'/brands/skill-graphics/skill-graphics-2.png'} />
-            <AnimatedCard
-                title={
-                    <>
-                        MVRK <br /> STUDIOS{' '}
-                    </>
-                }
-                description={
-                    <>
-                        <p className="transition-all duration-700 ease-out delay-100 group-hover:translate-y-0">Visual Effects</p>
-                        <p className="transition-all duration-700 ease-out delay-200 group-hover:translate-y-0">Motion Graphics</p>
-                    </>
-                }
-                url="https://mvrk.tv/"
-            />
-            <ServiceImage src={'/brands/plus/plus-1.png'} />
-            <ServiceImage src={'/brands/auleek/auleek-1.png'} />
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <ServiceImage src={"/brands/skill-graphics/sk-2.jpg"} />
+      <AnimatedCard
+        title={
+          <>
+            MVRK <br /> STUDIOS{" "}
+          </>
+        }
+        description={
+          <>
+            <p className="transition-all duration-700 ease-out delay-100 group-hover:translate-y-0">
+              Visual Effects
+            </p>
+            <p className="transition-all duration-700 ease-out delay-200 group-hover:translate-y-0">
+              Motion Graphics
+            </p>
+          </>
+        }
+        url="https://mvrk.tv/"
+      />
+      <ServiceImage src={"/brands/plus/plus-1.png"} />
+      <ServiceImage src={"/brands/auleek/auleek-1.png"} />
+    </div>
+  );
 };
 
 const ColumnFour = () => {
-    return (
-        <div className="flex flex-col gap-4 items-center justify-center">
-            <ServiceImage src={'/brands/skill-graphics/skill-graphics-4.png'} />
-            <div className="relative border border-gray-50/50 rounded-lg overflow-hidden">
-                <video
-                    src="/brands/mvrk/mvrk-vid.mp4"
-                    autoPlay
-                    loop
-                    muted
-                    className="w-[500px] md:w-[430px] h-[250px] object-cover rounded-lg"
-                />
-                <div className="absolute inset-0 bg-[#401444]/40" />
-            </div>
-            <AnimatedCard
-                title={
-                    <>
-                        PLUS ONLINE <br /> MARKETING
-                    </>
-                }
-                description={
-                    <>
-                        <p className="transition-all duration-700 ease-out delay-100 group-hover:translate-y-0">
-                            Search Engine Optimization
-                        </p>
-                        <p className="transition-all duration-700 ease-out delay-100 group-hover:translate-y-0">Content Management</p>
-                        <p className="transition-all duration-700 ease-out delay-200 group-hover:translate-y-0">
-                            Website Design & Development
-                        </p>
-                        <p className="transition-all duration-700 ease-out delay-200 group-hover:translate-y-0">Performance Marketing</p>
-                    </>
-                }
-                url="https://plusonlinemarketing.com/"
-            />
-            <ServiceImage src={'/brands/plus/plus-2.png'} />
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <ServiceImage src={"/brands/skill-graphics/sk-4.jpg"} />
+      <div className="relative border border-gray-50/50 rounded-lg overflow-hidden">
+        <video
+          src="/brands/mvrk/mvrk-vid.mp4"
+          autoPlay
+          loop
+          muted
+          className="w-[500px] md:w-[430px] h-[250px] object-cover rounded-lg"
+        />
+        <div className="absolute inset-0 bg-[#401444]/40" />
+      </div>
+      <AnimatedCard
+        title={
+          <>
+            PLUS ONLINE <br /> MARKETING
+          </>
+        }
+        description={
+          <>
+            <p className="transition-all duration-700 ease-out delay-100 group-hover:translate-y-0">
+              Search Engine Optimization
+            </p>
+            <p className="transition-all duration-700 ease-out delay-100 group-hover:translate-y-0">
+              Content Management
+            </p>
+            <p className="transition-all duration-700 ease-out delay-200 group-hover:translate-y-0">
+              Website Design & Development
+            </p>
+            <p className="transition-all duration-700 ease-out delay-200 group-hover:translate-y-0">
+              Performance Marketing
+            </p>
+          </>
+        }
+        url="https://plusonlinemarketing.com/"
+      />
+      <ServiceImage src={"/brands/plus/plus-2.png"} />
+    </div>
+  );
 };
